649275036f1abd708de0e24feb2e5c892395e9ce
huafei yu <lx12900mn@gmail.com>
2024-08-08 12:46:47 +0800

    main.py应用的添加

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import cv2
+
 from src import *
-# label = 9
-#
-# file = 'E:/Cloud/realpoint2/side4.ply'
-# destfile = 'data/side4.ply'
+label = 11
+
+# file = 'E:/Cloud/realpoint2/under.ply'
+# destfile = 'data/under.ply'
 #
 # plydata = get_plydata_ply_xyz(file)
 #
@@ -11,7 +13,25 @@ from src import *
 # plydata = set_label(plydata, label)
 #
 # plydata.write(destfile)
-file0 = 'data/des.ply'
-file1 = 'data/side4.ply'
+
+# file0 = 'data/des.ply'
+# file1 = 'data/under.ply'
 # fuse_ply_des(file0, file1)
-extract_label_dest(file0,0)
+
+
+# extract_label_dest(file0,0)
+
+
+# a = 'asdfsdf'
+# print(list(a))
+
+# file = "C:/Users/Yu-hf/Pictures/Screenshots/a.png"
+# img = cv2.imread(file)
+# img = cv2.resize(img,(338,255))
+# cv2.imwrite(file,img)
+
+import os
+
+file = os.listdir(r'data')
+
+print(file)
